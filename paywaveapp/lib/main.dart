163d2585e2415c6bcaf2a325f14767d2337fdf21
deveshtiwari <tiwaridevesh419@gmail.com>
2024-01-07 18:44:19 +0530

    updated

--- paywaveapp/lib/main.dart
+++ paywaveapp/lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: HomePage(), // Note: Changed 'Homepage()' to 'HomePage()'
+      // drawer: HomePage(), // Note: Changed 'Homepage()' to 'HomePage()'
       appBar: AppBar(
         title: const Text('Home Page'),
       ),

6d052c0f8b34e62b859acaaa4c500e0483610cbf
devesh <tiwaridevesh419@gmail.com>
2024-01-17 23:19:22 +0530

    commited

--- paywaveapp/lib/Pages/LoginPage.dart
+++ paywaveapp/lib/Pages/LoginPage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -8,6 +7,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 38, 83, 120),
       ),
@@ -40,7 +40,10 @@ class LoginScreen extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: [Color.fromARGB(255, 38, 83, 120), Color.fromARGB(255, 38, 83, 120)],
+          colors: [
+            Color.fromARGB(255, 38, 83, 120),
+            Color.fromARGB(255, 38, 83, 120)
+          ],
           stops: [0.5, 0.5],
         ),
       ),
@@ -51,7 +54,6 @@ class LoginScreen extends StatelessWidget {
           fontFamily: 'sans-serif',
           color: Colors.white,
           fontSize: 34.0,
-          
         ),
       ),
     );
@@ -78,7 +80,7 @@ class LoginScreen extends StatelessWidget {
 //     return Container(
 //       padding: const EdgeInsets.all(20.0),
 //       child: Column(
-        
+
 //         mainAxisAlignment: MainAxisAlignment.center,
 //         children: [
 //           const TextField(
@@ -151,96 +153,97 @@ class LoginScreen extends StatelessWidget {
 //   }
 // }
   Widget _buildLoginForm(BuildContext context) {
-  return Container(
-    padding: const EdgeInsets.all(16.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const TextField(
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.grey,
-              ),
-            ),
-            hintText: 'Username or Email',
-            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          ),
-        ),
-        const SizedBox(height: 20),
-        const TextField(
-          obscureText: true,
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.grey,
-              ),
-            ),
-            hintText: 'Password',
-            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          ),
-        ),
-        const SizedBox(height: 20),
-        Row(
-          children: [
-            Checkbox(
-              value: false, // Set the initial value as needed
-              onChanged: (bool? value) {
-                // Handle checkbox changes here
-              },
-            ),
-            const Text(
-              'Remember Me',
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            primary: const Color.fromARGB(255, 38, 66, 120),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: const Text('LOG IN', style: TextStyle(color: Colors.white)),
-        ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Forgot Password?',
-                style: TextStyle(
-                  color: Colors.blue,
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const TextField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
                 ),
               ),
+              hintText: 'Username or Email',
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 20),
             ),
-            const SizedBox(width: 10),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'New to Bank Apps? Sign Up',
-                style: TextStyle(
-                  color: Colors.blue,
+          ),
+          const SizedBox(height: 20),
+          const TextField(
+            obscureText: true,
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
                 ),
               ),
+              hintText: 'Password',
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 20),
             ),
-          ],
-        ),
-      ],
-    ),
-  );
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Checkbox(
+                value: false, // Set the initial value as needed
+                onChanged: (bool? value) {
+                  // Handle checkbox changes here
+                },
+              ),
+              const Text(
+                'Remember Me',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: const Color.fromARGB(255, 38, 66, 120),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text('LOG IN', style: TextStyle(color: Colors.white)),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'New to Bank Apps? Sign Up',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
-
 
 void main() {
   runApp(const MaterialApp(
